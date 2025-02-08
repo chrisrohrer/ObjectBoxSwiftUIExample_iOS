ad6705fb3d3afe48c54a14c2c06465210a86fab6
Christoph Rohrer <christoph@rohrers.de>
2025-02-08 13:03:50 +0100

    Initial Commit

---
+++ ObjectBoxSwiftUIExample_iOS/ObjectBoxSwiftUIExample_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectBoxSwiftUIExample_iOSApp.swift
+//  ObjectBoxSwiftUIExample_iOS
+//
+//  Created by Christoph Rohrer on 08.02.25.
+//
+
+import SwiftUI
+
+@main
+struct ObjectBoxSwiftUIExample_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
